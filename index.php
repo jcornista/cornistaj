7fc2d4cdc11a07fb1a3cf20949e9b9756d52dc2d
jcornista <53499705+jcornista@users.noreply.github.com>
2019-07-31 09:39:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
       <header>
         <h1>Jonah Cornista</h1>
         <h2>BSIT-1</h2>
+        <footer>
+          <p1>By: Jonah Cornista</p>
+        </footer>
       </header>
     </article>
   </main>
